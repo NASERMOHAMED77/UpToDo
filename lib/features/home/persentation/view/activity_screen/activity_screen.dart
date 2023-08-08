315a6d033bf3cb74b149bab4764ac6200cea6004
nasser <sssern89@gmail.com>
2023-08-08 11:15:27 +0300

    edit

--- lib/features/home/persentation/view/activity_screen/activity_screen.dart
+++ lib/features/home/persentation/view/activity_screen/activity_screen.dart
@@ -1,6 +1,4 @@
-import 'package:firee/features/home/persentation/manager/post_screen_cubit/post_screen_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ActivityScreenBody extends StatelessWidget {
   const ActivityScreenBody({
